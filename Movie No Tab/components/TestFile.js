3377756ae2fe2da0297aa6b9ba90c1784935b83e
byhhh2 <rlaalsdl111@gmail.com>
2021-02-23 21:43:55 +0900

    no tab

---
+++ Movie No Tab/components/TestFile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+class TestFile extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>TestFile</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default TestFile;

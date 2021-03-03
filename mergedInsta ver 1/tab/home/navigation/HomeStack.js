1bd87d91e8e2d6173210752505119f9b010ce306
rlafl7942 <46741373+rlafl7942@users.noreply.github.com>
2021-03-04 00:59:48 +0900

    file organization

--- mergedInsta ver 1/tab/home/navigation/HomeStack.js
+++ mergedInsta ver 1/tab/home/navigation/HomeStack.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeScreen from '../../components/HomeScreen';
-import CommentScreen from '../../components/CommentScreen';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import React from "react";
+import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import HomeScreen from "../screens/HomeScreen";
+import CommentScreen from "../screens/CommentScreen";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const HeaderIcon = (props) => {
   return (
@@ -35,19 +35,20 @@ class HomeStack extends React.Component {
             component={HomeScreen}
             options={{
               headerStyle: {
-                backgroundColor: 'white',
+                backgroundColor: "white",
               },
-              headerTintColor: 'black',
+              headerTintColor: "black",
               headerTitleStyle: {
-                fontFamily: 'Norican-Regular',
+                fontFamily: "Norican-Regular",
                 fontSize: 30,
               },
               headerRight: () => (
                 <View
                   style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}>
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
                   <HeaderIcon name="ios-add-circle-outline" />
                   <HeaderIcon name="ios-heart-outline" />
                   <HeaderIcon name="ios-paper-plane-outline" />
@@ -58,18 +59,19 @@ class HomeStack extends React.Component {
           <Stack.Screen
             name="CommentScreen"
             component={CommentScreen}
-            options={({navigation}) => ({
-              headerTitle: '댓글',
-              headerTitleAlign: 'center',
+            options={({ navigation }) => ({
+              headerTitle: "댓글",
+              headerTitleAlign: "center",
               headerStyle: {
-                backgroundColor: 'white',
+                backgroundColor: "white",
               },
-              headerTintColor: 'black',
+              headerTintColor: "black",
               headerLeft: () => (
                 <TouchableOpacity
                   onPress={() => {
                     navigation.goBack();
-                  }}>
+                  }}
+                >
                   <HeaderIcon name="ios-chevron-back" />
                 </TouchableOpacity>
               ),

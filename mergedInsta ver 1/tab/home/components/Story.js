369379c6b86c626210f12196035f4889aa633647
rlafl7942 <46741373+rlafl7942@users.noreply.github.com>
2021-03-31 00:30:18 +0900

    Add image swipe, story

--- mergedInsta ver 1/tab/home/components/Story.js
+++ mergedInsta ver 1/tab/home/components/Story.js
@@ -1,21 +1,38 @@
+import axios from 'axios';
 import React from 'react';
+import {PureComponent} from 'react';
 import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-
+import StoryScreen from '../screens/StoryScreen';
 //ICON
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Story = (props) => (
-  <View style={styles.storyId}>
-    <TouchableOpacity>
-      <Ionicons name="ios-person-circle-outline" size={65} color="black" />
-    </TouchableOpacity>
-    <Text style={{fontSize: hp('1.6%'), color: 'black'}}>{props.name}</Text>
-  </View>
-);
+export default class Story extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View>
+        <View style={styles.storyId}>
+          <TouchableOpacity onPress={this.props.modalHandler}>
+            <Ionicons
+              name="ios-person-circle-outline"
+              size={65}
+              color="black"
+            />
+          </TouchableOpacity>
+          <Text style={{fontSize: hp('1.6%'), color: 'black'}}>
+            {this.props.name}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   storyId: {
@@ -27,5 +44,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-export default Story;
---
+++ mergedInsta ver 1/tab/home/screens/StoryScreen.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import {Button} from 'native-base';
+import React from 'react';
+import {PureComponent} from 'react';
+import {View, TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+//ICON
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export default class StoryScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {story: []};
+    this.getStory = this.getStory.bind(this);
+  }
+  componentDidMount() {
+    this.getStory();
+  }
+  getStory() {
+    axios
+      .get(`${axios.defaults.baseURL}/stories/james`, {
+        headers: {
+          Authorization: axios.defaults.headers.common['Authorization'],
+        },
+      })
+      .then((response) => {
+        //console.log(response.data);
+        this.setState({
+          story: response.data.data,
+        });
+        console.log(this.state.story);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.background}
+          onPress={this.props.modalHandler}
+        />
+        <View style={styles.modalView}>
+          <Image
+            style={styles.imgView}
+            source={{
+              uri: `http://34.64.201.219:8080/api/v1/uploads/1617113394286_5.jpg`,
+              //왜 ${this.state.story.url} 안됨?
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    height: hp('100%'),
+    width: wp('100%'),
+    backgroundColor: 'transparent',
+  },
+  background: {
+    position: 'absolute',
+    height: hp('100%'),
+    width: wp('100%'),
+    backgroundColor: 'rgba(0,0,0,0.8)',
+  },
+  modalView: {
+    display: 'flex',
+    marginHorizontal: wp('5%'),
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: hp('80%'),
+  },
+  imgView: {
+    height: hp('50%'),
+    width: wp('100%'),
+    //backgroundColor: 'blue',
+  },
+});

9820f5196986a86991eb203e7c5e9bb1fab1ecc4
jeongbinboo <jrkasey3461@naver.com>
2021-04-04 14:42:07 +0900

    jeongbin_magnifier commit

--- mergedInsta ver 1/tab/magnifier/components/Files.js
+++ mergedInsta ver 1/tab/magnifier/components/Files.js
@@ -1,71 +1,101 @@
-import React from 'react';
-import {StyleSheet, View, ScrollView, Image} from 'react-native';
+import axios from 'axios';
+import React, {Component} from 'react';
+import {TouchableHighlight} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+import {StyleSheet, View, ScrollView, Image, FlatList} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-class Files extends React.Component {
+export default class Files extends React.Component {
+  state = {
+    data: [],
+    page: 1,
+    refreshing: false,
+  };
+  _renderItem = ({item}) => (
+    <React.Fragment>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={styles.touch}
+          onPress={navigation.navigate('Login')}>
+          <Image
+            source={{
+              uri: `http://34.64.201.219:8080/api/v1/uploads/${this.state.data[0]}`,
+            }}
+            style={styles.pict}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.touch}>
+          <Image
+            source={{
+              uri: `http://34.64.201.219:8080/api/v1/uploads/${this.state.data[1]}`,
+            }}
+            style={styles.pict}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.touch}>
+          <Image
+            source={{
+              uri: `http://34.64.201.219:8080/api/v1/uploads/${this.state.data[2]}`,
+            }}
+            style={styles.pict}
+          />
+        </TouchableOpacity>
+      </View>
+    </React.Fragment>
+  );
+  _getData = () => {
+    axios
+      .get(`${axios.defaults.baseURL}files`, {
+        headers: {
+          Authorization: axios.defaults.headers.common['Authorization'],
+        },
+      })
+      .then((response) => {
+        const rdn = Math.floor(Math.random() * 18);
+        this.setState({
+          data: this.state.data.concat(response.data.data[rdn].url),
+          page: this.state.page + 1,
+          refreshing: false,
+        });
+      });
+  };
+  _handleRefresh = () => {
+    this.setState(
+      {
+        refreshing: true,
+        page: 1,
+      },
+      this._getData,
+    );
+  };
+  componentDidMount() {
+    this._getData();
+  }
+  _handleLoadMore = () => {
+    this._getData();
+  };
   render() {
     return (
-      <ScrollView>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/최준.jpg')} style={styles.pict} />
-          <Image
-            source={require('../files/사쿠란보.jpg')}
-            style={styles.pict}
-          />
-          <Image source={require('../files/떡볶이.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/한지민.jpg')} style={styles.pict} />
-          <Image
-            source={require('../files/브랜드별치킨.jpg')}
-            style={styles.pict}
-          />
-          <Image source={require('../files/쿠팡.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/돈까스.jpg')} style={styles.pict} />
-          <Image source={require('../files/이특.jpg')} style={styles.pict} />
-          <Image source={require('../files/polo.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/이종석.jpg')} style={styles.pict} />
-          <Image source={require('../files/test.jpg')} style={styles.pict} />
-          <Image source={require('../files/하윤철.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/최준.jpg')} style={styles.pict} />
-          <Image
-            source={require('../files/사쿠란보.jpg')}
-            style={styles.pict}
-          />
-          <Image source={require('../files/떡볶이.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/한지민.jpg')} style={styles.pict} />
-          <Image
-            source={require('../files/브랜드별치킨.jpg')}
-            style={styles.pict}
-          />
-          <Image source={require('../files/쿠팡.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/돈까스.jpg')} style={styles.pict} />
-          <Image source={require('../files/이특.jpg')} style={styles.pict} />
-          <Image source={require('../files/polo.jpg')} style={styles.pict} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Image source={require('../files/이종석.jpg')} style={styles.pict} />
-          <Image source={require('../files/test.jpg')} style={styles.pict} />
-          <Image source={require('../files/하윤철.jpg')} style={styles.pict} />
-        </View>
-      </ScrollView>
+      <FlatList
+        data={this.state.data}
+        renderItem={this._renderItem}
+        keyExtractor={(item, index) => item.id}
+        onEndReached={this._handleLoadMore}
+        onEndReachedThreshold={1}
+        refreshing={this.state.refreshing}
+        onRefresh={this._handleRefresh}
+      />
     );
   }
 }
+
 const styles = StyleSheet.create({
+  touch: {
+    flex: 1,
+  },
   pict: {
-    width: '33%',
+    width: '100%',
     height: 150,
     margin: 1,
   },
 });
-export default Files;

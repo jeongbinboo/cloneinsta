fd93dd5a3af57554fa1907b45c40737e8b463dca
rlafl7942 <46741373+rlafl7942@users.noreply.github.com>
2021-04-30 19:22:31 +0900

    Add storyProfileImage

--- mergedInsta ver 1/tab/home/components/Story.js
+++ mergedInsta ver 1/tab/home/components/Story.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import {PureComponent} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {View, TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -15,13 +15,33 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 export default class Story extends PureComponent {
   constructor(props) {
     super(props);
+    this.state = {profileImage: ''};
     this.onPressFunc = this.onPressFunc.bind(this);
+    this.getUserProfile = this.getUserProfile.bind(this);
+  }
+  componentDidMount() {
+    this.getUserProfile();
   }
   onPressFunc() {
     //this.props.storyHandler();
     this.props.navigation.navigate('StoryScreen', {
-      writer: this.props.writer,
+      writer: this.props.item.writer,
     });
+  }
+  getUserProfile() {
+    axios
+      .get(`${axios.defaults.baseURL}/users/${this.props.item.writer}`, {
+        headers: {
+          Authorization: axios.defaults.headers.common['Authorization'],
+        },
+      })
+      .then((response) => {
+        this.setState({profileImage: response.data.data[0].profile_image});
+        console.log(this.state.profileImage);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   render() {
     return (
@@ -31,14 +51,21 @@ export default class Story extends PureComponent {
             onPress={() => {
               this.onPressFunc();
             }}>
-            <Ionicons
-              name="ios-person-circle-outline"
-              size={65}
-              color="black"
+            <Image
+              style={{
+                height: 55,
+                width: 55,
+                borderRadius: 60,
+                marginBottom: 7,
+                marginTop: 5,
+              }}
+              source={{
+                uri: `http://34.64.201.219:8080/api/v1/uploads/${this.state.profileImage}`,
+              }}
             />
           </TouchableOpacity>
           <Text style={{fontSize: hp('1.6%'), color: 'black'}}>
-            {this.props.writer}
+            {this.props.item.writer}
           </Text>
         </View>
       </View>
